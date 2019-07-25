f3420672a99848160b1ace8724c37daea867e2ca
REN XIANG <Seeumt.cumt.edu.cn>
2019-07-26 00:59:12 +0800

    加油！！！！！

---
+++ src/Try.java
@@ -0,0 +1,13 @@
+public class Try {
+    public static void main(String[] args) {
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+        System.out.println("你认为会成功上传到GitHub么");
+    }
+}

989f72982db3fb033baa95ecbb4a27523f63846c
REN XIANG <Seeumt@cumt.edu.cn>
2019-07-26 01:41:14 +0800

    能不能成功

--- src/Try.java
+++ src/Try.java
@@ -2,12 +2,5 @@ public class Try {
     public static void main(String[] args) {
         System.out.println("你认为会成功上传到GitHub么");
         System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
-        System.out.println("你认为会成功上传到GitHub么");
     }
 }
